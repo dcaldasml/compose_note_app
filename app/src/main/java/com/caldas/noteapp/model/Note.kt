592425489bbcbce74839ead84f6dfa2313f7ec2f
Douglas Caldas <douglas.caldas@mercadolivre.com>
2022-02-13 17:59:11 -0300

    Adding Room model setup

--- app/src/main/java/com/caldas/noteapp/model/Note.kt
+++ app/src/main/java/com/caldas/noteapp/model/Note.kt
@@ -1,11 +1,24 @@
 package com.caldas.noteapp.model
 
-import java.time.LocalDateTime
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+import java.util.Date
 import java.util.UUID
 
+@Entity(tableName = "TB_NOTE")
 data class Note(
+    @PrimaryKey
+    @ColumnInfo(name = "NOTE_ID")
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "NOTE_TITLE")
     val title: String,
+
+    @ColumnInfo(name = "NOTE_DESCRIPTION")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name = "NOTE_ENTRY_DATE")
+    val entryDate: Date = Date.from(Instant.now())
 )
