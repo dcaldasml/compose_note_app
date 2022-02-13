da1d9cc60c01efddab0b822af2f7feddc9789aef
Douglas Caldas <douglas.caldas@mercadolivre.com>
2022-02-13 18:12:57 -0300

    Adding coroutines scope to NoteDatabaseDao

--- app/src/main/java/com/caldas/noteapp/data/NoteDatabaseDao.kt
+++ app/src/main/java/com/caldas/noteapp/data/NoteDatabaseDao.kt
@@ -2,26 +2,27 @@ package com.caldas.noteapp.data
 
 import androidx.room.*
 import com.caldas.noteapp.model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDatabaseDao {
 
     @Query("SELECT * FROM TB_NOTE")
-    fun getNotes(): List<Note>
+    fun getNotes(): Flow<List<Note>>
 
     @Query("SELECT * FROM TB_NOTE WHERE NOTE_ID = :id")
-    fun getNoteById(id: String): Note
+    suspend fun getNoteById(id: String): Note
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(note: Note)
+    suspend fun insert(note: Note)
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun update(note: Note)
+    suspend fun update(note: Note)
 
     @Query("DELETE FROM TB_NOTE")
-    fun deleteAll()
+    suspend fun deleteAll()
 
     @Delete
-    fun deleteNote(note: Note)
+    suspend fun deleteNote(note: Note)
 
 }
